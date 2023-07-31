98d510a84d46563f3dc5f1a48083700fd30e9260
irumaru <jibakomaorochi13@outlook.jp>
2023-07-31 14:02:54 +0000

    ドメイン名とhttpプロトコルを環境変数から取得

--- in_auth/auth_2.1/config.php
+++ in_auth/auth_2.1/config.php
@@ -4,8 +4,13 @@
 //Service関係
 //配列的に$config_serviceと$config_serviceUrlは同順である必要があります。
 //サービスはDBに登録する必要があります。
-$config_service=['admin', 'iass', 'cu_admin', 'matchResult'];//設定可能サービス
-$config_serviceUrl=['accountAdmin.php', 'http://iass.binarymonster.net/index.php', 'http://cu-admin.binarymonster.net/index.php', 'https://matchResult.binarymonster.net?p=gameList'];//サービスURL
-$config_url='http://auth.binarymonster.net/';//wwwroot
-$cookiedomain='binarymonster.net';//cookieアクセス可能ドメイン
+
+//環境変数から取得
+$domain = getenv('HTTP_S_DOMAIN');
+$schema = getenv('HTTP_SCHEMA');
+
+$config_service = ['admin', 'iass', 'cu_admin', 'matchResult'];//設定可能サービス
+$config_serviceUrl = ['accountAdmin.php', $schema.'://iass.'.$domain.'/index.php', $schema.'://cu-admin.'.$domain.'/index.php', $schema.'://matchResult.'.$domain.'?p=gameList'];//サービスURL
+$config_url = $schema.'://auth.'.$domain.'/';//wwwroot
+$cookiedomain = $domain;//cookieアクセス可能ドメイン
 ?>
--- in_iass/iass_5.0/config.php
+++ in_iass/iass_5.0/config.php
@@ -1,6 +1,11 @@
 <?php
 //データベース接続情報はdb.phpに記述
 //データ保存場所
+
+//環境変数から取得
+$domain = getenv('HTTP_S_DOMAIN');
+$schema = getenv('HTTP_SCHEMA');
+
 $cfg_datadir='/mnt/persistent-volume/iass/data/';
 $cfg_dustbox='/mnt/persistent-volume/iass/dustbox/';
 $cfg_uploadtmp='/mnt/persistent-volume/iass/uploadtmp/';
@@ -9,7 +14,7 @@ $cfg_session='/var/www/in_auth/auth_2.1/session.php';
 $cfg_session_multiacces='/var/www/in_auth/auth_2.1/session-multiacces.php';
 $cfg_ffprobe='/var/www/in_iass/ffmpeg-4.2.2-i686-static/ffprobe';
 $cfg_ffmpeg='/var/www/in_iass/ffmpeg-4.2.2-i686-static/ffmpeg';
-$cfg_session_logout='https://auth.binarymonster.net/logout.php?target=iass';//URL
+$cfg_session_logout=$schema.'://auth.'.$domain.'/logout.php?target=iass';//URL
 $cfg_qualityDisplay=['1080p'=>'最高品質', '1080p[1440x1080]'=>'高品質', '720p'=>'中品質', '720p[960x720]'=>'中低品質', '480p'=>'低品質'];
 $cfg_qualityEncode=['1080p'=>['width'=>1920, 'height'=>1080], '1080p[1440x1080]'=>['width'=>1440, 'height'=>1080], '720p'=>['width'=>1280, 'height'=>720], '720p[960x720]'=>['width'=>960, 'height'=>720], '480p'=>['width'=>640, 'height'=>360]];
 $cfg_bgiTrueExtension=['jpg', 'png', 'gif'];
