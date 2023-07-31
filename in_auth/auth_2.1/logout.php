b8509ad764fd936ce85eceb6993741dae7422f50
irumaru <jibakomaorochi13@outlook.jp>
2023-07-31 14:18:39 +0000

    logoutのエラーを修正

--- in_auth/auth_2.1/logout.php
+++ in_auth/auth_2.1/logout.php
@@ -1,3 +1,18 @@
+<?php
+//設定ファイルの読み込み
+require_once 'config.php';
+//宣言
+global $cookiedomain;
+
+if(isset($_COOKIE['sessionkey']) && isset($_GET['target'])){
+    setcookie('sessionkey', '', -3600, '/', $cookiedomain);
+    $mes = '<p>ログアウトしました。<br><span id="move"></span>秒後にログイン画面に移動します。</p>';
+    $target=$_GET['target'];
+}else{
+    header('http', true, 404);
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,22 +21,10 @@
     <title>ログアウト</title>
 </head>
 <body>
-<?php
-//宣言
-global $cookiedomain;
-
-if(isset($_COOKIE['sessionkey']) && isset($_GET['target'])){
-    setcookie('sessionkey', null, -3600, '/', $cookiedomain);
-    echo '<p>ログアウトしました。<br><span id="move"></span>秒後にログイン画面に移動します。</p>';
-    $target=$_GET['target'];
-}else{
-    header('http', true, 404);
-    exit();
-}
-?>
+<?php echo $mes ?>
 </body>
 <script type="text/javascript">
-var i=5;
+var i=3;
 
 window.onload=function(){
     move();
