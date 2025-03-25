0b9657149f42cc4e1e8a49473960693d25f98f97
pantyetta <email@pantyetta.com>
2025-03-25 16:51:24 +0900

    animeCard update w-full

--- web/app/components/animeCard.tsx
+++ web/app/components/animeCard.tsx
@@ -4,14 +4,14 @@ import { Skeleton } from "./ui/skeleton";
 type CardArgs = {
   Title: string;
   Description?: string;
-  href: string
+  href: string;
 };
 
 export const AnimeCard = ({ Title, Description, href }: CardArgs) => {
   return (
-    <div className="">
-      <Link to={href}>
-        <Skeleton className="h-[170px] w-[288px]" />
+    <div className="w-full p-1">
+      <Link to={href} className="w-full h-full">
+        <Skeleton className="w-full aspect-video" />
         <Skeleton className="mt-3 h-3 w-[250px]" />
         <Skeleton className="mt-1 h-3 w-[150px]" />
       </Link>
--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { FaClockRotateLeft } from "react-icons/fa6";
-import { PiArrowsClockwiseBold } from "react-icons/pi";
+import { PiArrowsClockwiseBold, PiChatTextBold } from "react-icons/pi";
 import { AnimeCard } from "~/components/animeCard";
 
 export const meta: MetaFunction = () => {
@@ -18,13 +18,7 @@ export default function Index() {
           <PiArrowsClockwiseBold />
           更新されたアニメ
         </p>
-        <div className="flex flex-wrap w-full gap-x-4 gap-y-6 justify-center my-4">
-          <AnimeCard Title="test" Description="test" href="/play/1" />
-          <AnimeCard Title="test" Description="test" href="/play/1" />
-          <AnimeCard Title="test" Description="test" href="/play/1" />
-          <AnimeCard Title="test" Description="test" href="/play/1" />
-          <AnimeCard Title="test" Description="test" href="/play/1" />
-          <AnimeCard Title="test" Description="test" href="/play/1" />
+        <div className="grid grid-cols-1 w-full gap-x-4 gap-y-6 my-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <AnimeCard Title="test" Description="test" href="/play/1" />
           <AnimeCard Title="test" Description="test" href="/play/1" />
           <AnimeCard Title="test" Description="test" href="/play/1" />
@@ -36,17 +30,23 @@ export default function Index() {
           <FaClockRotateLeft />
           履歴
         </p>
-        <div className="flex flex-wrap w-full gap-x-4 gap-y-6 justify-center my-4">
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
-          <AnimeCard Title="test" Description="test" href="/season/1"/>
+        <div className="grid grid-cols-1 w-full gap-x-4 gap-y-6 my-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <AnimeCard Title="test" Description="test" href="/play/1" />
+          <AnimeCard Title="test" Description="test" href="/play/1" />
+          <AnimeCard Title="test" Description="test" href="/play/1" />
+          <AnimeCard Title="test" Description="test" href="/play/1" />
+        </div>
+      </div>
+      <div>
+        <p className="text-lg ml-4 mt-8 flex gap-2 items-center">
+          <PiChatTextBold />
+          おすすめ
+        </p>
+        <div className="grid grid-cols-1 w-full gap-x-4 gap-y-6 my-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <AnimeCard Title="test" Description="test" href="/season/1" />
+          <AnimeCard Title="test" Description="test" href="/season/1" />
+          <AnimeCard Title="test" Description="test" href="/season/1" />
+          <AnimeCard Title="test" Description="test" href="/season/1" />
         </div>
       </div>
     </div>
